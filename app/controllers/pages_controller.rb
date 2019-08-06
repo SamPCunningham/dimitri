1b8ea4f474a646a05b65793156194220abeeeee8
Samuel Cunningham <samuel.p.cunningham@gmail.com>
2019-08-06 15:53:31 +0000

    completed quiz by adding simple form and display for entered items

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def index
+    @pages = Page.all
   end
+
+  def new
+    @page = Page.new
+  end
+
+  def create
+    Page.create(page_params)
+    redirect_to root_path
+  end
+
+
+  private
+
+  def page_params
+    params.require(:page).permit(:name, :description, :function)
+  end  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_143744) do
+ActiveRecord::Schema.define(version: 2019_08_06_153448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "machines", force: :cascade do |t|
+    t.string "name"
+    t.string "function"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
     t.string "name"
     t.string "function"
     t.text "description"
